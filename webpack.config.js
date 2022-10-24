ed14913a3929cc9796a349a48259fb81246decaa
tottran <tottianddona@gmail.com>
2022-10-24 17:40:06 +0700

    set to development mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: {
     bundle: path.resolve(__dirname, 'src/index.js'),//[bundle] is a bundle's name
   },
