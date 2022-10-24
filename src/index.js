88b62ec66ab9d11aedf83d1a0bfe5b38261c43cb
tottran <tottianddona@gmail.com>
2022-10-24 23:42:51 +0700

    add and set img src from indexjs

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import generateJokes from './generateJoke';
 import '../styles/main.scss';
 import colorBrand from './assets/colorBrand.png';
 
+const colorBrandEl = document.getElementById('colorBrandEl');
+colorBrandEl.src = colorBrand;
+
 console.log(generateJokes());
